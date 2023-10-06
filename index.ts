e2269113172da10797c5bdd27317714d06966191
Jurijs Kovzels <yuri.kovzel@gmail.com>
2023-10-06 12:48:04 +0200

    adding ts

---
+++ index.ts
@@ -0,0 +1,17 @@
+console.log("up and running");
+
+let input = <HTMLInputElement> document.getElementById('linkInput');
+
+async function onInput(event: InputEvent){
+	const value = input.value;
+	console.log("new value:", value);
+
+	let url = value; //validate url
+
+	let response = await fetch(url, {mode: 'cors', });
+	console.log(response.status, response.statusText);
+	const reader = response.body.getReader();
+	
+}
+
+input.addEventListener("input", onInput);
